d478d3eab08a9097daeb407d2d5e9d0cda36eec1
AjMcKenzie <am312792@students.davenport.k12.ia.us>
2017-02-08 12:46:09 -0600

    some eclipse things
    I dont know what thisw is maybe so new text

---
+++ DelgadosTacos/src/orderingMenu/Tacos.java
@@ -0,0 +1,117 @@
+/**
+ * 
+ */
+package orderingMenu;
+import java.util.Scanner;
+/**
+ * @author AM312792
+ *
+ */
+public class Tacos {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		tacoTruck();
+		menu();
+		orderingMenu();
+
+		
+		
+	}
+	public static void orderingMenu()
+	{
+		
+		String burrito, taco, churro, enchilasas, tamales, tacoSalad, pop;
+		double burritoPrice = 1.99;
+		double tacoPrice = 0.99;
+		double popPrice = 0.99;
+		double popOrdered;
+		double tacoOrdered;
+		double burritoOrdered;
+		double totalPrice;
+		double totalPriceWithTax;
+	
+
+		
+		
+		Scanner inputDevice = new Scanner(System.in);
+		
+		System.out.print("How many tacos do you want? >>>> ");
+		tacoOrdered = inputDevice.nextDouble();
+		inputDevice.nextLine();
+		
+		System.out.print("How many burritos do you want? >>>> ");
+		burritoOrdered = inputDevice.nextDouble();
+		inputDevice.nextLine();
+			
+		System.out.print("Do you want a pop? >>>> ");
+		popOrdered = inputDevice.nextDouble();
+		inputDevice.nextLine();
+		
+		totalPrice = (tacoPrice * tacoOrdered) + (burritoOrdered * burritoPrice);
+		
+		System.out.println("You ordered " + tacoOrdered + " tacos, " + burritoOrdered + " burritos, pop, for a total of $" + totalPrice);
+		
+		totalPriceWithTax = totalPrice * 1.075;
+		System.out.println("Total with tax is $" + totalPriceWithTax);	
+		
+		
+	}
+	public static void menu()
+	{
+		System.out.println("                                  ");
+		System.out.println("                                  ");
+		System.out.println("                                  ");
+		System.out.println("                                  ");
+		System.out.println("**********************************");
+		System.out.println("         Tacos price $0.99        ");
+		System.out.println("        Burrito price $1.99       ");
+		System.out.println("          Pop price $0.99         ");
+		System.out.println("                                  ");
+		System.out.println("**********************************");
+		System.out.println("                                  ");
+		System.out.println("                                  ");
+		
+	
+	
+	
+	
+	
+	
+	
+	}	
+	public static void tacoTruck()
+	{
+		System.out.println("                              sss                                                                                                 ");
+		System.out.println("                             s   s                                                                                                ");
+		System.out.println("                            s     s                                                                                               ");
+		System.out.println("                           s       s                                                                                              ");
+		System.out.println("                 sssssssssssssssssssssssssssss                                                                                    ");
+		System.out.println("               ssssssssss           sssssssssss                                                                                   ");
+		System.out.println("   ***********************************************************                                                                    ");
+		System.out.println("   *         _____  ____     ___    ___                      *                                                                    ");
+		System.out.println("   *           |   |____|   |      |   |                     *                                                                    ");
+		System.out.println("   *           |   |    |   |___   |___|                     *                                                                    ");
+		System.out.println("   *                                                         *                                                                    ");
+		System.out.println("   *         ****************************                    *                                                                    ");
+		System.out.println("   *         *          ooo             *                    *                                                                    ");
+		System.out.println("   *         *         o   o            *                    *                                                                    ");
+		System.out.println("   *         *          ooo V           *                    ****************                                                     ");
+		System.out.println("   *         *           | /            *                    *     *         *                                                    ");
+		System.out.println("   *         *          /|              *                    *     *          *                                                   ");
+		System.out.println("   *         ****************************                    *     *           *                                                  ");
+		System.out.println("   *                                                         *     *************                                                  ");
+		System.out.println("   *                                                         *                 *                                                  ");
+		System.out.println("   *                                                         *                 *                                                  ");
+		System.out.println("   *                                                         *                 *                                                  ");
+		System.out.println("   *                                                         *******************                                                  ");
+		System.out.println("   *********ooooo**************************ooooo*************                                                                     ");
+		System.out.println("          oo     oo                      oo     oo                                                                                ");
+		System.out.println("         oo       oo                    oo       oo                                                                               ");
+		System.out.println("          oo     oo                      oo     oo                                                                                ");
+		System.out.println("            ooooo                          ooooo                                                                                  ");
+		}
+}
