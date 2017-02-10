3ab688585cea1f76869c3bf85370c8e06e505e51
AjMcKenzie <am312792@students.davenport.k12.ia.us>
2017-02-10 13:30:53 -0600

    youDon'tDoIt

---
+++ DecisionMaking/src/decisionMaking/doorPrize.java
@@ -0,0 +1,45 @@
+package decisionMaking;
+import java.util.Scanner;
+/**
+ * @author AM312792
+ *
+ */
+public class doorPrize 
+{
+
+	public static void main(String[] args) 
+	{
+		int choice;
+		Scanner input = new Scanner(System.in);
+		System.out.print("Choose a door between 1 and 3! >>>> ");
+		choice = input.nextInt();
+		
+		//door number one
+		if(choice == 1)
+		{
+			System.out.println("You win a brand new car!");
+			
+		}
+		if(choice == 2)
+		{
+			System.out.println("You win an old pair of socks!");
+			
+		}
+		if(choice == 3)
+		{
+			System.out.println("You win two, two way tickets to Norway!");
+			
+		}
+		if(choice >= 4)
+		{
+			System.out.println("Didn't you read the instructions? There are only three doors, so you get nothing!"); 
+		}
+		/**if(choice = 1 || 2 || 3)
+		{
+			System.out.println("You win a brand new car!"); || System.out.println("You win an old pair of socks!"); || System.out.println("You win two, two way tickets to Norway!");
+		}*/
+		
+
+	}
+
+}
